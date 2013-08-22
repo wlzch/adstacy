16f6e41837688ced23e185ddeaf00642ed4df52f
wlzch <wilzichi92@gmail.com>
2013-08-22 23:12:32 +0700

    Add setThumbHeight to LoadPostData.

--- src/Ads/AppBundle/DataFixtures/ORM/LoadPostData.php
+++ src/Ads/AppBundle/DataFixtures/ORM/LoadPostData.php
@@ -9,16 +9,21 @@ class LoadPostData extends DataFixture
 {
     public function load(ObjectManager $manager)
     {
+        $posts = array();
         for ($i = 1; $i <= 32; $i++) {
             $tags = $this->faker->words($this->faker->randomNumber(0, 10));
             $definedTags = array('promo', 'jual');
             $tags = array_merge($tags, $definedTags);
             $description = $this->faker->sentence($this->faker->randomNumber(1, 10)).implode('#', $tags);
             $image = $this->getReference('image-'.$i);
+            $size = getimagesize($image->getFile());
+            $height = round((234 / $size[0]) * $size[1]);
             $post = new Post();
             $post->setImage($image);
             $post->setDescription($description);
             $post->setWall($this->getReference('wall-'.$this->faker->randomNumber(1, 15)));
+            $post->setThumbHeight($height);
+            $posts[] = $post;
             $manager->persist($post);
             $this->addReference('post-'.$i, $post);
         }
