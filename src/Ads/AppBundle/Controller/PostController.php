0a71715904c2789efa3c638962e64f95638ea9e3
wlzch <wilzichi92@gmail.com>
2013-08-22 22:28:34 +0700

    Optimizing code. Save thumbnail height when adding new post.

--- src/Ads/AppBundle/Controller/PostController.php
+++ src/Ads/AppBundle/Controller/PostController.php
@@ -16,17 +16,34 @@ class PostController extends Controller
      */
     public function addAction()
     {
+        $request = $this->getRequest();
+        $filter = 'thumbnail';
         $post = new Post();
         $form = $this->createForm(new PostType(), $post, array(
             'username' => $this->getUser()->getUsername() 
         ));
-        $form->handleRequest($this->getRequest());
+        $form->handleRequest($request);
         $wallForm = $this->createForm(new WallType(), new Wall(), array(
             'action' => $this->generateUrl('ads_app_wall_add') 
         ));
 
         if ($form->isValid()) {
             $em = $this->getManager();
+            $em->persist($post);
+            $em->flush();
+
+            $storage = $this->get('vich_uploader.storage');
+            $cacheManager = $this->get('liip_imagine.cache.manager');
+            $dataManager = $this->get('liip_imagine.data.manager');
+            $filterManager = $this->get('liip_imagine.filter.manager');
+
+            $image = $post->getImage();
+            $origPath = $storage->resolveUri($image, 'file'); // resolves real file uri (vich)
+            $this->get('liip_imagine.controller')->filterAction($request, $origPath, $filter); // store the file
+
+            $targetPath = str_replace($request->getBaseUrl(), '', $cacheManager->generateUrl($origPath, $filter)); // get thumbnail file path
+            $size = $dataManager->find($filter, $targetPath)->getSize();
+            $post->setThumbHeight($size->getHeight());
             $em->persist($post);
             $em->flush();
         }
