1b6737b08f0ba421bab7ead550aeddb1cd47a3d9
wlzch <wilzichi92@gmail.com>
2013-08-26 13:32:32 +0700

    Refactor ajaxlink.

--- src/Adstacy/AppBundle/Resources/public/js/module/ajaxlink.js
+++ src/Adstacy/AppBundle/Resources/public/js/module/ajaxlink.js
@@ -1,8 +1,32 @@
 (function ($) {
-  $.fn.ajaxlink = function(options) {
-    var settings = $.extend({}, options);
+  $.fn.ajaxlink = function(type) {
+    var types = {
+      ads: {
+        countSelector: '.promotes-count',
+        jsonField: 'promotes_count',
+        firstSelector: 'a.promote',
+        secondSelector: 'a.unpromote'
+      },
+      show_wall: {
+        countSelector: '.followers-count',
+        jsonField: 'followers_count',
+        firstSelector: 'a.follow-wall',
+        secondSelector: 'a.unfollow-wall'
+      },
+      show_ad: {
+        firstSelector: 'a.follow-wall',
+        secondSelector: 'a.unfollow-wall'
+      },
+      user_profile: {
+        countSelector: '.user-followers-count',
+        jsonField: 'followers_count',
+        firstSelector: 'a.follow-user',
+        secondSelector: 'a.unfollow-user'
+      }
+    };
 
     return this.each(function() {
+      var settings = types[type];
       var $parent = $(this);
 
       $parent.find(settings.firstSelector+','+settings.secondSelector).click(function(event) {
