9edd0850028f54916885ccb42600eee6dcdd9e79
wlzch <wilzichi92@gmail.com>
2013-08-26 09:27:17 +0700

    Add profile picture twig helper.

---
+++ src/Adstacy/AppBundle/Twig/UserExtension.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Adstacy\AppBundle\Twig;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Adstacy\AppBundle\Entity\User;
+
+/**
+ * User extension
+ */
+class UserExtension extends \Twig_Extension
+{
+    private $container;
+    
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function getFunctions()
+    {
+        return array(
+            'profile_pic' => new \Twig_Function_Method($this, 'getProfilePicture', array('is_safe' => array('html'))),
+        );
+    }
+
+    /**
+     * get user profile picture
+     */
+    public function getProfilePicture(User $user)
+    {
+        $uploaderHelper = $this->container->get('vich_uploader.templating.helper.uploader_helper');
+        $cacheManager = $this->container->get('liip_imagine.cache.manager');
+        $image = $user->getImage();
+        if ($image) {
+            return $cacheManager->getBrowserPath($uploaderHelper->asset($image, 'file'), 'profile_pic');
+        }
+        if ($user->getProfilePicture()) {
+            return $user->getProfilePicture();
+        }
+
+        return $this->container->getParameter('no_profpic_img');
+    }
+
+    public function getName()
+    {
+        return 'adstacy_user_extension';
+    }
+}
+
