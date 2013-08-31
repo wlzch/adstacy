2c64518702f632f35e4c150229573046827fe43a
Erwin <rw7nz.cyruz@gmail.com>
2013-08-31 17:34:42 +0700

    custom contact us form.

--- src/Adstacy/AppBundle/Form/Type/ContactType.php
+++ src/Adstacy/AppBundle/Form/Type/ContactType.php
@@ -10,19 +10,24 @@ class ContactType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options = array())
     {
-      $builder->add('subject', 'text', array(
-        'label' => 'form_contact.subject',
-        'required' => false
-      ));
-      $builder->add('email', 'email', array(
-        'label' => 'form_contact.email'
-      ));
-      $builder->add('content', 'textarea', array(
-        'label' => 'form_contact.content'
-      ));
-      $builder->add('submit','submit', array(
-        'label' => 'form_contact.submit'
-      ));
+
+        $builder->add('subject', 'text', array(
+            'label' => 'form_contact.subject',
+            'required' => false
+        ));
+        $builder->add('email', 'email', array(
+            'label' => 'form_contact.email'
+        ));
+        $builder->add('content', 'textarea', array(
+            'label' => 'form_contact.content',
+            'attr' => array(
+                'rows' => 5
+            )
+        ));
+        $builder->add('submit','submit', array(
+            'label' => 'form_contact.submit'
+        ));
+
     }
 
     public function setDefaultOptions(OptionsResolverInterface $resolver)
