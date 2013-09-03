67af128d4777723dc9e92fe2999e835724ad9321
wlzch <wilzichi92@gmail.com>
2013-09-03 20:13:24 +0700

    Fix error in ad show when not logged in.

--- src/Adstacy/AppBundle/Controller/AdController.php
+++ src/Adstacy/AppBundle/Controller/AdController.php
@@ -20,7 +20,7 @@ class AdController extends Controller
         if (!$ad) {
             throw $this->createNotFoundException();
         }
-        $adsByUser = $this->getRepository('AdstacyAppBundle:Ad')->findByUser($this->getUser(), 6);
+        $adsByUser = $this->getRepository('AdstacyAppBundle:Ad')->findByUser($ad->getUser(), 6);
 
         return $this->render('AdstacyAppBundle:Ad:show.html.twig', array(
             'ad' => $ad,
