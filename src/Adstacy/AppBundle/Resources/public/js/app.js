69d73c4428a9d3ea7d4a20f9057fc3e52fa94eff
wlzch <wilzichi92@gmail.com>
2013-09-08 12:32:22 +0700

    Add cross browser placeholder.

--- src/Adstacy/AppBundle/Resources/public/js/app.js
+++ src/Adstacy/AppBundle/Resources/public/js/app.js
@@ -8,12 +8,34 @@
     )
     document.getElementsByTagName("head")[0].appendChild(msViewportStyle)
   }
+  if(!Modernizr.input.placeholder){
+    $('[placeholder]').focus(function() {
+      var input = $(this);
+      if (input.val() == input.attr('placeholder')) {
+        input.val('');
+        input.removeClass('placeholder');
+      }
+    }).blur(function() {
+      var input = $(this);
+      if (input.val() == '' || input.val() == input.attr('placeholder')) {
+        input.addClass('placeholder');
+        input.val(input.attr('placeholder'));
+      }
+    }).blur();
+    $('[placeholder]').parents('form').submit(function() {
+      $(this).find('[placeholder]').each(function() {
+        var input = $(this);
+        if (input.val() == input.attr('placeholder')) {
+          input.val('');
+        }
+      })
+    });
+  }
   var $masonry = $('.masonry');
   var $adstacyContainer = $('.adstacy-container');
   var $window = $(window);
   var $searchContainer = $('#search-container');
   var windowWidth = $window.width();
-  console.log(windowWidth);
 
   if (windowWidth >= 480) {
     $masonry.masonry({
