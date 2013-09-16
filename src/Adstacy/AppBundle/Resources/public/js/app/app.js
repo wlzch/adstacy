1e03dd546f3495ee31651a7d0631aa1e8044405f
Erwin <rw7nz.cyruz@gmail.com>
2013-09-16 13:42:14 +0700

    drop support for newline on comment.

--- src/Adstacy/AppBundle/Resources/public/js/app/app.js
+++ src/Adstacy/AppBundle/Resources/public/js/app/app.js
@@ -7,9 +7,7 @@
   $('.advert').ajaxlink('ads');
   $('.user').ajaxlink('users');
   $('.comment-box').keydown(function(event) {
-    if (event.which == 13 && event.shiftKey) {
-      return true;
-    } else if (event.which == 13) {
+    if (event.which == 13) {
       this.form.submit();
       return false;
     }
