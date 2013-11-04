d4fd8702e80763e963df44cbaefb835550d72e0d
wlzch <wilzichi92@gmail.com>
2013-11-04 20:31:30 +0700

    Add trending sidebar.

--- src/Adstacy/AppBundle/Twig/AdstacyExtension.php
+++ src/Adstacy/AppBundle/Twig/AdstacyExtension.php
@@ -20,12 +20,29 @@ class AdstacyExtension extends \Twig_Extension
     {
         return array(
             'is_ajax' => new \Twig_Function_Method($this, 'isAjax', array('is_safe' => array('html'))),
+            'render_sidebar_trending' => new \Twig_Function_Method($this, 'renderSidebarTrending', array(
+                'is_safe' => array('html')
+            )),
         );
     }
     
     public function isAjax()
     {
         return $this->container->get('request')->isXmlHttpRequest();
+    }
+
+    public function renderSidebarTrending()
+    {
+        $redis = $this->container->get('snc_redis.default');
+        $ids = $redis->lrange('trending', 0, -1);
+        $id = $ids[rand(0, count($ids) - 1)];
+        $ad = $this->container->get('doctrine')->getManager()->getRepository('AdstacyAppBundle:Ad')->findOneById($id);
+
+        return $this->container->get('templating')->render(
+            'AdstacyAppBundle:App:sidebar_trending.html.twig', array(
+                'ad' => $ad
+            )
+        );
     }
 
     public function getName()
