ed34097a67844a860e047704124eb7cd779bb9fd
wlzch <wilzichi92@gmail.com>
2013-11-14 16:03:36 +0700

    FIx load comments (AGAIN!)

--- src/Adstacy/AppBundle/Twig/AdstacyExtension.php
+++ src/Adstacy/AppBundle/Twig/AdstacyExtension.php
@@ -21,6 +21,7 @@ class AdstacyExtension extends \Twig_Extension
         return array(
             'is_ajax' => new \Twig_Function_Method($this, 'isAjax'),
             'is_mobile' => new \Twig_Function_Method($this, 'isMobile'),
+            'sort_comments' => new \Twig_Function_Method($this, 'sortComments'),
             'render_sidebar_trending' => new \Twig_Function_Method($this, 'renderSidebarTrending', array(
                 'is_safe' => array('html')
             )),
@@ -35,6 +36,25 @@ class AdstacyExtension extends \Twig_Extension
     public function isAjax()
     {
         return $this->container->get('request')->isXmlHttpRequest();
+    }
+
+    /**
+     * Sort comments
+     *
+     * @param array $comments
+     *
+     * @return array
+     */
+    public function sortComments($comments = array())
+    {
+        if (!is_array($comments)) {
+          $comments = $comments->toArray();
+        }
+        usort($comments, function($a, $b) {
+          return $a->getId() > $b->getId();
+        });
+
+        return $comments;
     }
 
     public function getName()
